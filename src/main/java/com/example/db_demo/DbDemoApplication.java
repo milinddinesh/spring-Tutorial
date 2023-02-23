5357ff8dcf9ae301b2835ca740d2e538e41e6e2a
Milind <milinddinesh@gmail.com>
2023-02-23 18:26:43 +0530

    a greeting function

--- src/main/java/com/example/db_demo/DbDemoApplication.java
+++ src/main/java/com/example/db_demo/DbDemoApplication.java
@@ -2,12 +2,21 @@ package com.example.db_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DbDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbDemoApplication.class, args);
+	}
+
+	@GetMapping(path = "/hello")
+	public String greeting(@RequestParam (defaultValue = "world")String text ){
+		return String.format("Hello %s",text);
 	}
 
 }
